99790c7f48e49db6b95aa7bd6218501148332856
DavidRbb <david_140789@hotmail.es>
2024-11-29 21:40:06 +0100

    Create davidrbb.py
    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/davidrbb.py
@@ -0,0 +1,34 @@
+# https://www.python.org/
+
+# Comentario en una línea
+
+"""
+Esto también es
+un comentairo
+en varias líneas
+"""
+
+'''
+Esto es
+un comentario
+en varias líneas
+'''
+
+my_variable = "Mi variable"
+my_variable = "Nuevo valor de mi variable"
+
+MY_CONSTANT = "Mi constante" # por convención
+MY_CONSTANT= "AKASKDSKAK"
+
+my_int = 1
+my_float = 1.5
+my_bool_ False
+my_string = "Mi cadena de texto"
+my_other_string = "Mi otra cadena de texto"
+
+print("Hola, Python!")
+
+print(type(my_int))
+print(type(my_float))
+print(type(my_bool))
+print(type(my_string))
